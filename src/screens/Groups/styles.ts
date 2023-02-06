dd71cc6a57728b536444c130729e30811590478b
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-06 17:01:18 -0300

    feat: add theme and @types for styled components

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,12 +4,12 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: #09090a;
+  background-color: ${(props) => props.theme.COLORS.GRAY_700};
 `;
 
 export const Text = styled.Text`
-  color: #e1e1e6;
-  font-size: 32px;
+  color: ${(props) => props.theme.COLORS.GRAY_100};
+  font-size: ${(props) => props.theme.FONT_SIZE.XL};
   text-transform: uppercase;
   letter-spacing: 2px;
 `;
