5122fcb7b28be9a30f9d4fc05664eab38de05338
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-06 16:29:50 -0300

    feat: setup styled-components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #09090a;
+`;
+
+export const Text = styled.Text`
+  color: #e1e1e6;
+  font-size: 32px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+`;
