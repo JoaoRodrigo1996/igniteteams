579dadd63f86321e7fe40745e349a28c5605eb98
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-06 19:46:10 -0300

    feat: add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import { Groups } from "@screens/Groups";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 
+import { Groups } from "@screens/Groups";
+
+import { Loading } from "@components/Loading";
 import theme from "@theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,14 +2,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: ${(props) => props.theme.COLORS.GRAY_700};
-`;
-
-export const Text = styled.Text`
-  color: ${(props) => props.theme.COLORS.GRAY_100};
-  font-size: ${(props) => props.theme.FONT_SIZE.XL};
-  text-transform: uppercase;
-  letter-spacing: 2px;
+  background-color: ${(props) => props.theme.COLORS.GRAY_600};
+  padding: 24px;
 `;
