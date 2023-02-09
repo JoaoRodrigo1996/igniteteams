1b5c425f3a9f7e6b9c9306f647e0130f56a7650a
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-09 19:07:21 -0300

    feat: create list empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from "react-native";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera do PUBG"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -19,6 +20,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma" />
+        )}
       />
     </Container>
   );
