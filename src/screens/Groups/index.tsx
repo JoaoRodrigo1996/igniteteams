420580558434ca23670f84fc3e673043fb95e554
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-09 18:19:24 -0300

    feat: list of groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera do PUBG"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subTitle="jogue com a sua turma" />
-      <GroupCard title="Galera do PUBG" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
