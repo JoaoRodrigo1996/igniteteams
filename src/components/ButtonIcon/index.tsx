4f61c746277886c2563ca0b133fdbc836928f3d4
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-10 18:44:32 -0300

    feat: dinamic icon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,14 +1,21 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { TouchableOpacityProps } from "react-native";
+
 import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
 
 interface ButtonIconProps extends TouchableOpacityProps {
   type?: ButtonIconTypeStyleProps;
+  icon: keyof typeof MaterialIcons.glyphMap;
 }
 
-export function ButtonIcon({ type = "PRIMARY", ...rest }: ButtonIconProps) {
+export function ButtonIcon({
+  icon,
+  type = "PRIMARY",
+  ...rest
+}: ButtonIconProps) {
   return (
     <Container {...rest}>
-      <Icon type={type} name="home" />
+      <Icon type={type} name={icon} />
     </Container>
   );
 }
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Input } from "@components/Input";
+import { Container, Form } from "./styles";
 
 export function Players() {
   return (
@@ -11,7 +12,11 @@ export function Players() {
         title="Nome da turma"
         subTitle="adicione a galera e separe os times"
       />
-      <ButtonIcon type="SECONDARY" />
+
+      <Form>
+        <Input placeholder="Nome do participante" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
