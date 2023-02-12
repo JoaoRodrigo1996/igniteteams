fdb5518ee9721c6c1d00b543f505f407e3cbc81b
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-02-12 15:39:17 -0300

    feat: implementing navigation

--- App.tsx
+++ App.tsx
@@ -3,20 +3,20 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 
-import { Players } from "@screens/Players";
-
 import { Loading } from "@components/Loading";
+import { Routes } from "@routes/index";
+
 import theme from "@theme/index";
-import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
